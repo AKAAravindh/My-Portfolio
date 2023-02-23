5e83c986a29753cbf7c8893f1dbe8abacfaa19e7
AKAAravindh <aravindhrajbca@gmail.com>
2023-02-23 23:57:32 +0530

    Portfolio v2.1

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-// gsap.registerPlugin(ScrollTrigger);
+let mainBody = document.getElementById('body')
 
-// gsap.to(".row", {
-//     x: 0,
-//     duration: 2,
-//     scrollTrigger: {
-//         trigger: ".row",
-//         start: "top center",
-//         markers: true,
-//         toggleAction: "restart none none none"
-//     },
+function visible() {
+    mainBody.style.display = "block"
+}
 
-// });
+setInterval(visible, 3000)
+
+const aboutSection = document.getElementById('section3');
+const row1 = document.getElementById('row1')
+const row2 = document.getElementById('row2')
+
+window.addEventListener('scroll', () => {
+    const sectionPos = aboutSection.getBoundingClientRect().top;
+    const screenPos = window.innerHeight;
+    console.log("Screen position =",sectionPos);
+
+    if (sectionPos < (screenPos / 2)) {
+        row1.style.display = 'inline-block'
+        row2.style.display = 'inline-block'
+    }
+    else {
+        row1.style.display = 'none'
+        row2.style.display = 'none'
+    }
+})
